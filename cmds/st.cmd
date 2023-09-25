034e645c691d20b65de4543a5b52b9bc5c8f9f6f
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2023-09-25 17:24:32 +0200

    Added db entries for autosave. Updated opi. Setup to use a daq6510 module.

--- cmds/st.cmd
+++ cmds/st.cmd
@@ -2,6 +2,20 @@
 
 require heaterctrl
 
-iocshLoad("$(daq6510_DIR)daq6510.iocsh", "IP=192.168.10.44,P=CstatV-AC:,IOCNAME=ioc27-htrctrl,TOP=/tmp/autosave")
+addScan(60)
+addScan(30)
 
-dbLoadRecords("heaterctrl.db", "P=CstatV-AC:, DEV=DAQ6510, PORT=PORT1")
+epicsEnvSet("IOCNAME", "ioc27-htrctrl")
+epicsEnvSet("IP", "192.168.10.44")
+epicsEnvSet("P", "CstatV-AC:")
+epicsEnvSet("TOP", "/tmp/autosave")
+
+#drvAsynIPPortConfigure("$(ASYN_PORT=PORT1)","$(IP):$(IP_PORT=5025)",0,0,0)
+iocshLoad("$(daq6510_DIR)daq6510_init.iocsh", "ASYN_PORT=DAQ_PORT,IP=192.168.10.44,P=CstatV-AC:")
+
+#dbLoadRecords("daq6510.db", "PORT=$(ASYN_PORT=PORT1),P=$(P),DEV=$(DEV=DAQ6510)")
+
+dbLoadRecords("asynRecord.db","P=${IOCNAME},R=:asynRec,PORT=$(ASYN_PORT=DAQ_PORT),ADDR='0',IMAX='1024',OMAX='256'")
+dbLoadRecords("heaterctrl.db", "P=CstatV-AC:, DEV=DAQ6510, PORT=DAQ_PORT")
+
+iocshLoad("$(autosave_DIR)/autosave.iocsh", "AS_TOP=$(TOP),IOCNAME=$(IOCNAME),IOCDIR=$(IOCDIR=daq6510)")
