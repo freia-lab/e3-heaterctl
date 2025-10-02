fff2bd8eadaaf54657005e6c0882d4ea5d5f6461
Marek Jacewicz <marek.jacewicz@physics.uu.se>
2025-10-02 11:37:18 +0200

    extra startup command file

---
+++ cmds/st-mj.cmd
@@ -0,0 +1,25 @@
+require heaterctrl
+
+#require recsync
+require iocstats
+
+addScan(60)
+addScan(30)
+
+epicsEnvSet("IOCNAME", "ioc28-htrctrl")
+epicsEnvSet("IP", "192.168.10.44")
+epicsEnvSet("P", "CstatV-AC")
+#epicsEnvSet("TOP", "/opt/epics/autosave")
+
+iocshLoad("$(daq6510_DIR)daq6510_init.iocsh", "ASYN_PORT=DAQ_PORT,IP=192.168.10.44,P=$(P)")
+
+dbLoadRecords("asynRecord.db","P=${IOCNAME},R=:asynRec,PORT=$(ASYN_PORT=DAQ_PORT),ADDR='0',IMAX='1024',OMAX='256'")
+dbLoadRecords("heaterctrl.db", "P=$(P):, DEV=DAQ6510, PORT=DAQ_PORT")
+dbLoadRecords("selection.db")
+
+#iocshLoad("$(autosave_DIR)/autosave.iocsh", "AS_TOP=$(TOP),IOCNAME=$(IOCNAME),IOCDIR=$(IOCDIR=daq6510)")
+
+# iocStats database
+dbLoadRecords("$(iocstats_DB)/iocAdminSoft-freia.db","IOC=$(IOCNAME)")
+
+
